5f9364fea0ed13bab9d5755f7300708532fd59af
copforrsa <148183144+copforrsa@users.noreply.github.com>
2026-09-24 14:59:52 -0400

    chore: refresh cache for Conquête rules and draw room

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_BUILD='50.31-role-by-pitch';
+const SW_BUILD='50.42-conquete-rules';
 const CACHE_NAME='swe-pwa-'+SW_BUILD;
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
